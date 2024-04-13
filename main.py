ee8b88f9bc28f987b42350fa953f93b09c093fb3
Khang Tran <firzen1912@gmail.com>
2024-04-12 21:31:41 -0700

    Format and styling improvement

--- main.py
+++ main.py
@@ -1,40 +1,9 @@
 # main.py
 
 from flask import Flask, render_template, request
-import requests
-from bs4 import BeautifulSoup
-from accessibility_scorer import calculate_accessibility_scores
+from web_scrap import scrape_website
 
 app = Flask(__name__)
-
-def scrape_website(url):
-    # Send a GET request to the URL
-    response = requests.get(url)
-    # Parse the HTML content
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    # Extract the name of the page and its title element
-    page_name = soup.find('title').get_text()
-    page_title = soup.title.string
-    
-    # Extract the heading structure of the page
-    headings = soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
-    heading_structure = [(heading.name, heading.text.strip()) for heading in headings]
-    
-    # Extract the alt text on each of the images on the page
-    images = soup.find_all('img')
-    image_alt_text = [(image['src'], image.get('alt')) for image in images]
-    
-    # Calculate accessibility scores
-    accessibility_scores = calculate_accessibility_scores(heading_structure, image_alt_text, page_title)
-    
-    return {
-        'page_name': page_name,
-        'page_title': page_title,
-        'heading_structure': heading_structure,
-        'image_alt_text': image_alt_text,
-        'accessibility_scores': accessibility_scores
-    }
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
---
+++ web_scrap.py
@@ -0,0 +1,56 @@
+# web_scrap.py
+
+import requests
+from bs4 import BeautifulSoup
+from accessibility_scorer import calculate_accessibility_scores
+
+def scrape_website(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
+    # Parse the HTML content
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Extract the name of the page and its title element
+    page_name = soup.find('title').get_text()
+    page_title = soup.title.string
+    
+    # Extract the heading structure of the page
+    headings = soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+    heading_structure = [(heading.name, heading.text.strip()) for heading in headings]
+    
+    # Extract the alt text on each of the images on the page
+    images = soup.find_all('img')
+    image_alt_text = [(image['src'], image.get('alt')) for image in images]
+    
+    
+    # Extract videos from direct <video> elements
+    videos = []
+    video_elements = soup.find_all('video')
+    for video in video_elements:
+        video_source = video.get('src')
+        video_alt = video.get('alt')
+        videos.append({'source': video_source, 'alt': video_alt})
+    
+    # Extract videos from embedded iframes
+    iframe_elements = soup.find_all('iframe')
+    for iframe in iframe_elements:
+        # Check if the iframe contains a video source
+        if 'youtube' in iframe['src']:
+            video_id = iframe['src'].split('/')[-1]
+            videos.append({'source': 'https://www.youtube.com/embed/' + video_id, 'alt': ''})
+        elif 'vimeo' in iframe['src']:
+            video_id = iframe['src'].split('/')[-1]
+            videos.append({'source': 'https://player.vimeo.com/video/' + video_id, 'alt': ''})
+        # Add other video hosting platforms as needed
+        
+    # Calculate accessibility scores
+    accessibility_scores = calculate_accessibility_scores(heading_structure, image_alt_text, page_title)
+    
+    return {
+        'page_name': page_name,
+        'page_title': page_title,
+        'heading_structure': heading_structure,
+        'image_alt_text': image_alt_text,
+        'videos': videos,
+        'accessibility_scores': accessibility_scores
+    }
